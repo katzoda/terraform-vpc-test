94f6c395194714461a596db55d083046dcc98cfa
katzoda <daniel.katzor@gmail.com>
2025-08-22 22:41:31 +0200

    Add resource private_subnet and variables.

--- main.tf
+++ main.tf
@@ -4,12 +4,23 @@ provider "aws" {
 }
 
 # VPC
-resource "aws_vpc" "test_vpc" {
+resource "aws_vpc" "terra_vpc" {
     cidr_block = var.vpc_cidr
     enable_dns_support = true
     enable_dns_hostnames = true
     tags = {
-        Name = "test_vpc"
+        Name = "terra_vpc"
+    }
+}
+
+# Private subnet
+resource "aws_subnet" "private_subnet" {
+    vpc_id = aws_vpc.terra_vpc.id
+    cidr_block = var.private_cidr_block
+    availability_zone = var.private_subnet_az
+    map_publip_ip_on_launch = false
+    tags = {
+        Name = "Private-eu-central-1a"
     }
 }
 
--- variables.tf
+++ variables.tf
@@ -6,3 +6,14 @@ variable "aws_region" {
 variable "vpc_cidr" {
     default = "10.5.0.0/16" 
 }
+
+variable "private_cidr_block" {
+    description = "CIDR for a private subnet"
+    default = "10.1.0.0/24"
+}
+
+variable "private_subnet_az" {
+    description = "AZ for private subnets"
+    default = "eu-central-1a"
+}
+
